779d1785c1487575d6c5507b234f9073a38a1cd1
Mikhail Dubov <mdubov@gmail.com>
2015-03-18 23:30:36 +0000

    Computed properties

--- app/models/album.js
+++ app/models/album.js
@@ -1,4 +1,6 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+
  var attr = DS.attr;
  var hasMany = DS.hasMany;
 
@@ -7,5 +9,14 @@ export default DS.Model.extend({
   name: attr('string'),
   artist: attr('string'),
   isExplicit: attr('boolean'),
-  songs: hasMany('songs')
-});
+  songs: hasMany('songs'),
+  totalDuration: function() {
+    var durations = this.get('songs').map(function(song){
+      return song.get('duration');
+    });
+    return durations.reduce(function(a, b) {
+      return a + b;
+    });
+  }.property('songs.@each.duration'),
+  songCount: Ember.computed.alias('songs.length')
+}); 
