d64b67aa14ab61fcda9accbd81ce1520bc56b6c6
Mikhail Dubov <mikhail@alphasights.com>
2015-03-19 11:31:35 +0000

    Add song-row component

---
+++ app/components/song-row.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'tr',
+  isPlaying: false,
+  actions: {
+    play: function() {
+      this.set('isPlaying', true);
+    },
+    pause: function() {
+      this.set('isPlaying', false);
+    }
+  }
+});
